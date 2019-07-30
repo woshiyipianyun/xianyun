afda713fe818a1ae31603cf3090054b374d43fd3
ojh <ojh@qq.com>
2019-07-30 21:26:08 +0800

    首页上传

---
+++ components/header.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="header">
+    <div class="box">
+      <el-row type="flex">
+        <!-- logo部分 -->
+        <div class="logo">
+          <h1>
+            <a href="/">
+              <img src="../assets/images/logo.jpg" alt />
+            </a>
+          </h1>
+        </div>
+        <!-- 头部导航部分 -->
+        <el-row type="flex" class="navs">
+          <nuxt-link to="/">首页</nuxt-link>
+          <nuxt-link to="/post">旅游攻略</nuxt-link>
+          <nuxt-link to="/hotel">酒店</nuxt-link>
+          <nuxt-link to="/air">国内机票</nuxt-link>
+        </el-row>
+
+        <!-- 登陆之后的个人中心 -->
+        <div class="message">
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              <i class="el-icon-message-solid"></i>
+              消息
+              <i class="el-icon-arrow-down"></i>
+            </span>
+            <!-- 注册登陆 -->
+            <span v-show="true">
+              <nuxt-link to="/user/login">登陆/注册</nuxt-link>
+            </span>
+            <!-- 登录之后 -->
+            <span v-show="false">
+              <nuxt-link to="/">个人中心</nuxt-link>
+              <nuxt-link to="/user/login">退出</nuxt-link>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>黄金糕</el-dropdown-item>
+              <el-dropdown-item>狮子头</el-dropdown-item>
+              <el-dropdown-item>螺蛳粉</el-dropdown-item>
+              <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+              <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </el-row>
+    </div>
+  </div>
+</template>
+ 
+<script>
+export default {};
+</script>
+<style lang="less" scoped>
+.header {
+  height: 60px;
+  line-height: 60px;
+  border-bottom: 1px solid #ccc;
+  box-shadow: 0 3px 3px #eee;
+  .box {
+    width: 1000px;
+    margin: 0 auto;
+    .logo {
+      img {
+        width: 150px;
+        height: 40px;
+        margin-top: 10px;
+      }
+    }
+    .navs {
+      flex: 1;
+      margin-left: 20px;
+      a {
+        display: block;
+        padding: 0 20px;
+        height: 60px;
+        line-height: 60px;
+        box-sizing: border-box;
+        &:hover {
+          background: #409eff;
+          color: #fff;
+        }
+      }
+    }
+    .nuxt-link-exact-active {
+      background: #409eff;
+    }
+    .message {
+      vertical-align: middle;
+    }
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <Header></Header>
+    <nuxt />
+
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+//导入头部公共组件
+import Header from "@/components/header.vue";
+import Footer from "@/components/footer.vue";
+export default {
+  //注册组件
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+
+<style>
+/* 公共样式初始化 */
+* {
+  margin: 0;
+  padding: 0;
+}
+
+ul,
+li,
+ol {
+  list-style: none;
+}
+a {
+  color: #666;
+  text-decoration: none;
+}
+em,
+i {
+  font-style: normal;
+}
+</style>
